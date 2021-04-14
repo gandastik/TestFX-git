fa338b14dfc5c0fb52a04f9d43d4daaf3e240f17
gandastik <gandastik@gmail.com>
2021-04-14 21:46:40 +0700

    add new feature

--- src/Controllers/ButtongController.java
+++ src/Controllers/ButtongController.java
@@ -2,6 +2,9 @@ package Controllers;
 
 public class ButtongController {
     public void something() {
-        System.out.println("eieiezaza");
+        System.out.println("alskdjf;lasjd;lfjasl;djf;lsjkadf");
+    }
+    public void feature() {
+        System.out.println("eieiza");
     }
 }
